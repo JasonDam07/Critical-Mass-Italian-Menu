6ea09299d7343e517d3cbe32ee3b57440312e7f0
Jason Dam <jason.dam.07@gmail.com>
2022-05-06 23:55:53 -0600

    Technical Section finished

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# TODO
+
+## 1. Detailed View
+
+This would be a more detailed view for each item when a user selects an item. This view could be a popup which displays an image of the dish and additional information such as approximately how much food the plate contains (in grams) and how much calories are in each plate, for those who are watching their calorie intake. Some other examples of additional information could be nutritional facts such as how much sodium, carbohydrates, protein, etc. are in a dish.
+
+## 2. More Filters
+
+More filters could be included. One idea for a filter is a search function which could look for a dish name or an ingredient in a dish. Other filters could filter out some common allergies/dietary restrictions. For example, if there are pastas with cream sauces, but a user is mildly lactose intolerant, they could filter out the cream sauce pastas.
+
+## 3. Order Preview
+
+Users could select the items they want to order and a label at the bottom of the screen can display the subtotal of the order. When the subtotal is pressed, the user could see their order and edit it from there (add more quantity of the same order or removing an order).
+
+## 4. Recommended Pairings
+
+This is a feature to go along the "order preview" feature. When a user selects an item, a brief popup confirms that the item has been added to the order tracker. A section of the confirmation popup will also show recommened pairings for the users ordered item. For example if a user adds an appetizers to their order, the recommendations could have a good main dish pairing or a good drink pairing.
+
+## 5. Returning Users
+
+If the users allow for cookies on the page, the menu could remember the users previous orders or most common orders and have a section that shows the users top orders. Another section could show orders that users have not tried before with the title "Try something new!". I believe that this is a feature that will be useful because people tend to have a habit to order dishes that they enjoy. This could also be useful for people who want to look for dishes that they have not tried before.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,101 @@
 import "./styles.css";
 import menu from "./menu";
 
+// set menu items
 const menuItems = menu.items;
 
-console.log(menuItems);
+// creates a menu array for each section
+// also stores te element information in a variable for each section
+let menuStarters = [];
+let starterElem = document.getElementById("starters");
+
+let menuPasta = [];
+let pastaElem = document.getElementById("pasta");
+
+let menuPizza = [];
+let pizzaElem = document.getElementById("pizza");
+
+// divide the menu items into seperate arrays
+menuItems.forEach(data => {
+  switch (data.type) {
+    case "starters":
+      menuStarters.push(data);
+      break;
+    case "pasta":
+      menuPasta.push(data);
+      break;
+    case "pizza":
+      menuPizza.push(data);
+      break;
+  }
+})
+
+// function created to sort each array using the "menuOrder" attribute of each menu item
+function sortMenu(_itemList) {
+  _itemList.sort((a, b) => {
+    return (a.menuOrder - b.menuOrder)
+  })
+}
+
+//run sort function for each menu array
+sortMenu(menuStarters);
+sortMenu(menuPasta);
+sortMenu(menuPizza);
+
+// function created to display the information on the html page
+function listItems(_itemList, _element) {
+
+  _itemList.forEach(item => {
+    let itemContainer = document.createElement("div");
+    let textContainer = document.createElement("div");
+    let menuItem = document.createElement("li");
+    let menuDesc = document.createElement("p");
+    let itemPrice = document.createElement("p");
+
+    if (item.spicy) {
+      itemContainer.className = "info_container filter";
+      menuDesc.className = "item_desc spicy";
+    } else {
+      itemContainer.className = "info_container";
+      menuDesc.className = "item_desc";
+    }
+
+    textContainer.className = `text_container`;
+    
+    menuItem.innerText = item.name;
+    menuItem.className = "item_name";
+
+    menuDesc.innerHTML = item.description;
+
+    itemPrice.className = "price_text";
+    itemPrice.innerHTML = " $" + item.price.toFixed(2);
+
+    textContainer.appendChild(menuItem);
+    textContainer.appendChild(menuDesc);
+
+    itemContainer.appendChild(textContainer);
+    itemContainer.appendChild(itemPrice);
+
+    _element.appendChild(itemContainer);
+  })
+}
+
+// use listItems function for each menu section to display the information on the html page
+listItems(menuStarters, starterElem);
+listItems(menuPasta, pastaElem);
+listItems(menuPizza, pizzaElem);
+
+// uses an event listener to see if the check box for the spicy filer is checked
+// if the checkbox is checked, set display style for every class "filter" to none
+document.getElementById('filter_spicy').addEventListener("change", function() {
+  let spicyItems = document.getElementsByClassName("filter")
+  for(let i = 0; i < spicyItems.length; i++){
+    if(document.getElementById('filter_spicy').checked){
+      spicyItems[i].style.display = "flex";
+    } else {
+      spicyItems[i].style.display = "none";
+    }
+  }
+});
+
+console.log(menuItems, menuStarters, menuPasta, menuPizza);
